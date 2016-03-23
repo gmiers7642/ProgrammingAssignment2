cdea4e4ec9433d1ed9625d462fb17b0eb75e8383
gmiers7642 <gmiers7642@gmail.com>
2016-03-22 23:11:15 -0500

    Replace code with correct version

--- cachematrix.R
+++ cachematrix.R
@@ -2,6 +2,8 @@
 # That is, if a matrix has already been inverted, it will be read from cache
 # instead of being inverted again
 
+# makeCacheMatrix(x)
+# Argument - matrix x to create CacheMatrix object from
 # This function constructs the CacheMatrix object, and will do the following:
 # 1) Set a matrix
 # 2) Get a matrix
@@ -12,28 +14,31 @@ makeCacheMatrix <- function(x = matrix()) {
   i <- NULL
   set <- function(y) {
     x <<- y
-    y <<- NULL
+    i <<- NULL
   }
-  get() <- function() x
-  setInverse() <- function(inv) i <<- inv
-  getInverse() <- function() i
-  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
+  get <- function() x
+  setInv <- function(inv) i <<- inv
+  getInv <- function() i
+  list(set = set, get = get,
+       setInv = setInv,
+       getInv = getInv)
 }
 
 
+# cacheSolve(x, ...)
+# Argument - matrix x to find an inverse for
 # This function calculates the inverse of a given matix
 # If the inverse already exists, read it
 # If it does not exist, calculate it
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-  i <- x&getInverse()
-  if(!is.null(i) ) {
-    message("Getting cached data")
+  i <- x$getInv()
+  if(!is.null(i)) {
+    message("getting cached data")
     return(i)
   }
   data <- x$get()
   i <- solve(data, ...)
-  x$setIverse(i)
+  x$setInv(i)
   i
 }
